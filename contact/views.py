0dc4a1931f35adc41fd46f841a95495e0a186078
emonhossain151 <ehosan151@gmail.com>
2024-10-28 20:50:13 +0600

    api added

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializer
 
 # Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = models.Contact.objects.all()
+    serializer_class = serializer.ContactSerializer
--- service/views.py
+++ service/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializer
 
 # Create your views here.
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = models.Service.objects.all()
+    serializer_class = serializer.ServiceSerializer
